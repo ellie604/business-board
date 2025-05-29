d06996d29b257c3b8246100052a76007eb2200aa
罗馨怡 <luoxinyi@MacBook-Air-224.local>
2025-05-28 22:36:53 -0400

    feat: reset database and add seed data

--- server/database.ts
+++ server/database.ts
@@ -1,13 +1,9 @@
-// server/database.mts
+// server/database.ts
 // import { PrismaClient } from './generated/prisma/client.js';
 // export const prisma = new PrismaClient();
 // database.mts
-import { PrismaClient as ProductionPrismaClient } from './generated/prisma-production';
-import { PrismaClient as PreviewPrismaClient } from './generated/prisma-preview';
+import { PrismaClient } from './generated/prisma-preview';
 
-// 默认使用 preview 数据库，除非明确指定使用 production
-const prisma = (process.env.NODE_ENV === 'production'
-  ? new ProductionPrismaClient()
-  : new PreviewPrismaClient()) as PreviewPrismaClient;
+const prisma = new PrismaClient();
 
 export { prisma };
