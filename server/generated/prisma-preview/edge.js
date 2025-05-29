3656834365e465055c54d5aab39a01e2b65d93ed
罗馨怡 <luoxinyi@MacBook-Air-224.local>
2025-05-28 21:48:44 -0400

    fix: update database and seed data

--- server/generated/prisma-preview/edge.js
+++ server/generated/prisma-preview/edge.js
@@ -202,12 +202,11 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
-  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
         "fromEnvVar": "PREVIEW_DATABASE_URL",
-        "value": null
+        "value": "postgresql://postgres.jklgdzqqwtbwrfgdyhxj:D&62Vhq7_4KCQgB@aws-0-us-east-2.pooler.supabase.com:5432/postgres"
       }
     }
   },
